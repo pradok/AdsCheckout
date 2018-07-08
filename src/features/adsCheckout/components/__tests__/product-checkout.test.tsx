0f6378c8a401f7037ff99f4651362ce9aca33953
Pradosh Sinha <pradoshks@outlook.com>
2018-07-08 15:33:37 +1000

    Checkout component with Cart created previously.

---
+++ src/features/adsCheckout/components/__tests__/product-checkout.test.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Product} from '../../models';
+import {shallow} from 'enzyme';
+
+
+import ProductCheckOut from '../product-checkout';
+
+it('<Product> renders correctly', () => {
+  const props: Product = {id: 1, price: 394.99, quantity: 379.99, priceReduced: 4, title: 'Premium Ad'};
+  const component = shallow(<ProductCheckOut {...props} />);
+  expect(component).toMatchSnapshot();
+});
---
+++ src/features/adsCheckout/components/product-checkout.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Product } from '../models';
+
+interface Props extends Product {
+}
+
+const ProductCheckout = ({price, quantity, priceReduced, title}: Props) => (
+  <div>
+    {title}
+    : &#36;{!priceReduced ? price : (priceReduced < 0 ? price : priceReduced)} {quantity ? ` x ${quantity}` : null}
+    = {priceReduced > 0 ? (quantity * priceReduced).toFixed(2) : (quantity * price).toFixed(2)}
+    {priceReduced < 0 ? `${priceReduced}` : ''}
+    {priceReduced < 0 ? `= $${+(quantity * price).toFixed(2) - (-priceReduced)}` : ''}
+  </div>
+);
+
+export default ProductCheckout;
